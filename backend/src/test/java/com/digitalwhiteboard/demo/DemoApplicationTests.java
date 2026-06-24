a4fa7e3acec70182622f24f1b1d1980c784af9db
Caspar Brantberger <brantbergercaspar@gmail.com>
2026-06-24 17:20:19 +0200

    chore: initialize DigitalWhiteBoard project

---
+++ backend/src/test/java/com/digitalwhiteboard/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalwhiteboard.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
